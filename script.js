24f595424a7bada7dccb31d36879f11881cfdbee
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2024-11-07 09:47:25 -0300

    07/11/2024 09:47

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-function Flipar(){
-    navigator.mediaDevices.getUserMedia({
-        video: {
-            facingMode: { exact: "environment" } // Para a câmera Traseira
-            // facingMode: "user" // Para a câmera Frontal
-        }
-    }).then(function (mediaStream) {
-        const video = document.querySelector('#video');
-        video.srcObject = mediaStream;
-        video.play();
-    });
-}
+let currentCamera = "user";
+var array = []
 
-function Flipar2(){
+function switchCamera() {
+    currentCamera = (currentCamera === "user") ? "environment" : "user";
     navigator.mediaDevices.getUserMedia({
         video: {
-            facingMode: "user" // Para a câmera Frontal
+            facingMode: currentCamera
         }
-    }).then(function (mediaStream) {
-        const video = document.querySelector('#video');
-        video.srcObject = mediaStream;
-        video.play();
-    });
+    })
+        .then(function (stream) {
+            const video = document.querySelector('video');
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function (error) {
+            console.error("Erro ao alternar câmeras: ", error);
+        });
 }
 
 document.querySelector('#capture').addEventListener('click', function (e) {
-    var canvas = document.querySelector("#canvas");  
+    if (localStorage.Rec) {
+        array = JSON.parse(localStorage.getItem("Rec"))
+    }
+    var canvas = document.querySelector("#canvas");
     canvas.height = video.videoHeight;
     canvas.width = video.videoWidth;
     var context = canvas.getContext('2d');
     context.drawImage(video, 0, 0)
+    const ImageData = canvas.toDataURL('image/png')
+    array.push({
+        "Title": document.getElementById("Title").value,
+        "Descrição": document.getElementById("description").value,
+        "Image": ImageData
+    })
+
+    localStorage.setItem("Rec", JSON.stringify(array))
 })
+
+var Conteudo = document.getElementById("Conteudo")
+
+function Load() {
+    var Buscar = JSON.parse(localStorage.getItem("Rec"))
+    Buscar.forEach(element => {
+        var Caixa = document.createElement("div")
+        Caixa.innerHTML = `
+        <h1>${element.Title}</h1>
+        <p>${element.Descrição}</p>
+        <img src="${element.Image}"/>
+        `
+        Conteudo.append(Caixa)
+    });
+}
+
+Load()
