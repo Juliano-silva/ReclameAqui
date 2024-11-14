61f77daaca734575f513d069b450e6346e27f55d
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2024-11-14 09:39:22 -0300

    14/11/2024

--- script.js
+++ script.js
@@ -47,11 +47,37 @@ document.querySelector('#capture').addEventListener('click', function (e) {
     }
 })
 
+function initMap() {
+    const location = { lat:this.id, lng: this.className }; // Example coordinates
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: location,
+    });
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+}
+
+
 var Conteudo = document.getElementById("Conteudo")
+
+function Remover() {
+    location.reload()
+   var Rec;
+   if (localStorage.getItem("Rec") == null) {
+       Rec = []
+   } else {
+       Rec = JSON.parse(localStorage.getItem("Rec"))
+   }
+   Rec.splice(this.id, 1)
+   localStorage.setItem("Rec", JSON.stringify(Rec))
+}
+
 
 function Load() {
     var Buscar = JSON.parse(localStorage.getItem("Rec"))
-    Buscar.forEach(element => {
+    Buscar.forEach(function(element,index){
         var Caixa = document.createElement("div")
         Caixa.innerHTML = `
         <h1>${element.Title}</h1>
@@ -59,9 +85,11 @@ function Load() {
         <img src="${element.Image}"/>
         <p>${element.Latitude}</p>
         <p>${element.Longetude}</p>
+        <button id="${element.Latitude}" className="${element.Longetude}">Localizar</button>
+        <button id="${index}" onclick="Remover()">Remover</button>
         `
         Conteudo.append(Caixa)
-    });
+    })
 }
 
 Load()
